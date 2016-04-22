696ee5d8a83728bfa5f20fd7d1923c0ec2dca046
Reva Scharf <rscharf@bu.edu>
2016-04-21 22:06:59 -0400

    user level for working lights

--- user/player.c
+++ user/player.c
@@ -5,12 +5,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-typedef struct
-{
-    int beat;
-    int lights;
-} BEAT_DATA;
-
 static long BEATS[1024];
 
 
@@ -45,46 +39,33 @@ int main(int argc, char **argv) {
     printf("tempstr 0 = %s\n", tempstr);
     printf("BEATS[0] = %lf\n", BEATS[0]);
     
-   // printf("BEATS[0] * 1000000 = %li\n", BEATS[0]);
     i = 1;
     while (fgets(tempstr, 15, fp)!=NULL)
     {
-        
-        //BEATS[i] = atof(tempstr);
         tempfloat = atof(tempstr);
         tempfloat = tempfloat * 1000000;
         BEATS[i] = tempfloat - prevNum;
         prevNum = tempfloat;
-//        BEATS[i] = tempfloat;
-  //      BEATS[i] = BEATS[i] - BEATS[i-1];
-        //BEATS[i] = BEATS[i] * 1000000; //convert to microseconds
         i++;
     }
     fclose(fp);
     printf("USER LEVEL: read all beats\n");
     //ALL beats are in the BEATS file.  now need to write it to /dev/mp3play
+	fclose(pFile);
     for (j=0; j<i; j++)
     {
+		pFile = fopen("/dev/mp3play", "r+");
         sprintf(tempstr, "%d", BEATS[j]);
         fputs(tempstr, pFile);
         printf("USER LEVEL: wrote BEATS[%d] = %li\n", j, BEATS[j]);
+		fclose(pFile);
     }
+	pFile = fopen("/dev/mp3play", "r+");
     printf("USER LEVEL: all beats written to kernel\n");
+	//sending the "R" will be once Caitie says to play the song
     fputs("R", pFile);
     
-    /*
-	if (argc == 2 && (atoi(argv[1]) != 0)) {
-		printf("\nValue\tSpeed\tState\tDirection\tBrightness\n");
-	    while(1)
-	    {
-	        fclose(pFile);
-	        pFile = fopen("/dev/mygpio", "r+");
-	    	fgets(line, 256, pFile);
-	    	printf("%s", line);
-	    	sleep(atoi(argv[1]));
-	    }
-	
-	}*/
+
 
 	fclose(pFile);
 	return 0;
